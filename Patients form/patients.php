f83b5c00a5c4b00cd4bd05ed4c79e9232ab76980
Rethabile <78046461+Rethabile-Code@users.noreply.github.com>
2021-06-18 13:55:26 +0200

    Initial Commit
    Eastern Cape Web system. 
    
    This is a System for the Eastern Cape Department of Health, we developed this as a software engineering student.
    It is for helping the Eastern Cape Department of Health in a fight against Covid-19, It have Staff and Management module.... Users can Track people with covid-19, Alert other provinces, Save patients data, Check available Beds and Ppes etc

---
+++ Patients form/patients.php
@@ -0,0 +1,45 @@
+<?php
+$firstname =$_POST['firstName'];
+$lastname =$_POST['lastName'];
+$idNumber =$_POST['id'];
+$dateOfBirth =$_POST['dateOfBirth'];
+$Gender=$_POST['Gender'];
+$phonenumber =$_POST['tel'];
+$Email=$_POST['email'];
+$province =$_POST['Province'];
+$Town =$_POST['Town'];
+$Address =$_POST['Address'];
+
+$host="localhost";
+$dbUsername="root";
+$dbPassword="";
+$dbname="coronavirus"; 
+//create connection
+$coo=new mysqli($host,$dbUsername,$dbPassword,$dbname);
+if(mysqli_connect_error()){
+	die('connection eror('. mysqli_connect_errno().')'. mysqli_connect_error());
+
+} 
+   else{
+   	 
+   	$INSERT="INSERT Into patients(firstname,lastname,id,dateOfBirth,Gender,tel,email,Province,Town,Address)VALUES(?,?,?,?,?,?,?,?,?,?) ";
+   	//statement
+   	$stmt=$conn->prepare($SELECT);
+   		$stmt->execute();
+   	$stmt->store_result();
+   $rnum= $stmt->num_rows();
+   	$stmt->close();
+   	$stmt =$conn->prepare($INSERT);
+   	$STMT->bindparam("ssssssssss",$firstname,$lastname,$id,$dateOfBirth,$Gender,$tel,$Email,$Province,$Town,$Address  );
+   
+   	echo "NEW PATIENT ADDED";
+
+   }
+   $stmt->close();
+   $conn->close();
+
+
+   }
+
+?>
+  
---
+++ login.html
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  
+
+    <title>َLOGIN HERE</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+<form class="box" action="#" method="POST">
+  <h1>Login</h1>
+
+  <div class="form_input">
+
+  <input type="text" name="username" placeholder="Username">
+
+</div>
+  <div class="form_input">
+
+  <input type="password" name="password" placeholder="Password">
+
+</div>
+
+  <input type="submit" name="submit" value="Login" class="btn-login">
+
+</form>
+</div>
+
+  </body>
+</html>
+
+
+
+
+<?php
+
+$conn=mysqli_connect("localhost","root","");
+mysqli_select_db($conn,"coronavirus");
+  
+  if(isset($_POST['submit'])){
+
+      $uname=$_POST['username'];
+      $password=$_POST['password'];
+
+      $sql=mysqli_query($conn,"select * from login where Username='".$uname."'AND Password='".$password."'");
+
+      $result=mysqli_fetch_array($sql);
+
+        if($password==$result['Password']){
+          header("location:patients.php");
+          exit();
+        }
+      else{
+         
+         echo "<script>alert('invalid details')</script>";
+        } 
+    }  
+?>
